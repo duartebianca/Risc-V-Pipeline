02361b95bc7009e33dabfd0e144d55db15744868
Matheus Barbosa <mamateubosa00@gmail.com>
2025-02-12 11:54:50 -0300

    chore: add new ALU operations: OR, SUB, SRLI, SRAI, and BLT

--- design/alu.sv
+++ design/alu.sv
@@ -17,10 +17,20 @@ module alu#(
             case(Operation)
             4'b0000:        // AND
                     ALUResult = SrcA & SrcB;
+            4'b0001:        // OR
+                    ALUResult = SrcA | SrcB;
             4'b0010:        // ADD
                     ALUResult = SrcA + SrcB;
-            4'b1000:        // Equal
+            4'b0011:        // SUB
+                    ALUResult = SrcA - SrcB;
+            4'b0100:        // SRLI
+                    ALUResult = SrcA >> SrcB;
+            4'b0101:        // SRAI
+                    ALUResult = SrcA >>> $signed(SrcB[4:0]);
+            4'b1000:        // BEQ
                     ALUResult = (SrcA == SrcB) ? 1 : 0;
+            4'b1001:        // BLT
+                    ALUResult = (SrcA < SrcB) ? 1 : 0;
             default:
                     ALUResult = 0;
             endcase
