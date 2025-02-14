a385fbafe6d900805836fbda09eecba0ad7ec265
Matheus Barbosa <mamateubosa00@gmail.com>
2025-02-14 11:24:35 -0300

    feat: add Halt signal support in Datapath, RISC_V, and RegPack modules

--- design/RISC_V.sv
+++ design/RISC_V.sv
@@ -19,7 +19,7 @@ module riscv #(
 );
 
   logic [6:0] opcode;
-  logic ALUSrc, MemtoReg, RegWrite, MemRead, MemWrite, Branch;
+  logic ALUSrc, MemtoReg, RegWrite, MemRead, MemWrite, Branch, Halt;
   logic [1:0] ALUop;
   logic [1:0] ALUop_Reg;
   logic [6:0] Funct7;
@@ -34,7 +34,8 @@ module riscv #(
       MemRead,
       MemWrite,
       ALUop,
-      Branch
+      Branch,
+      Halt
   );
 
   ALUController ac (
