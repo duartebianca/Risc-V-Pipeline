c57112883284659458fc0d6645198db31f3cc74a
Rafa Albert <albertc.rafaela@gmail.com>
2025-02-18 22:26:33 -0300

    feat: implement slt instruction

--- design/alu.sv
+++ design/alu.sv
@@ -19,8 +19,10 @@ module alu#(
                     ALUResult = SrcA & SrcB;
             4'b0010:        // ADD
                     ALUResult = SrcA + SrcB;
-            4'b1000:        // Equal
+            4'b0110:        // EQUAL
                     ALUResult = (SrcA == SrcB) ? 1 : 0;
+            4'b1100:        // LESS THAN
+                    ALUResult = (SrcA < SrcB) ? 1 : 0;
             default:
                     ALUResult = 0;
             endcase
